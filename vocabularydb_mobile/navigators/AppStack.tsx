49cb30ba7eb36f2946e1c1ef5f7ed5479c27ee4f
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-18 23:52:08 +0000

    Adding Authentication Navigator Logic

--- vocabularydb_mobile/navigators/AppStack.tsx
+++ vocabularydb_mobile/navigators/AppStack.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AuthenticationScreen from '../screens/AuthenticationScreen';
 import HomeScreen from '../screens/HomeScreen';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux-state/store';
 
 const Stack = createNativeStackNavigator();
 
 function AppStack() {
+  const isSigned = useSelector((state: RootState) => state.user.isSigned);
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="AuthenticationScreen"
-        component={AuthenticationScreen}
-      />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      {isSigned ? (
+        <>
+          <Stack.Screen
+            name="AuthenticationScreen"
+            component={AuthenticationScreen}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }

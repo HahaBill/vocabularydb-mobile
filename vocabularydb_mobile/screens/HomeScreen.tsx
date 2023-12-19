1bcd9c3e5ac92fb586d531fea957139ce37aac4b
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-19 23:55:47 +0000

    Added calling a lambda function in HomeScreen.tsx

--- vocabularydb_mobile/screens/HomeScreen.tsx
+++ vocabularydb_mobile/screens/HomeScreen.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux-state/store';
 
 function HomeScreen() {
-  return <Text>Hello from HomeScreen!</Text>;
+  const lambdaAPI =
+    'https://4k6jq6ypdpxyzmdnxul6i6y4ke0krgjw.lambda-url.us-east-1.on.aws/';
+
+  const userId = useSelector((state: RootState) => state.user.userId);
+  const [listTasks, setListTasks] = React.useState([]);
+
+  const getListTasks = async () => {
+    const response = await (
+      await fetch(`${lambdaAPI}/list-tasks/${userId}`)
+    ).json();
+    setListTasks(response.tasks);
+  };
+
+  React.useEffect(() => {
+    getListTasks();
+  });
+
+  const displayTasks = <div>{listTasks.map(task => task.content)}</div>;
+  return <View>{displayTasks}</View>;
 }
 
 export default HomeScreen;
